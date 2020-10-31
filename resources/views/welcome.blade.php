c1378904e4d336722d2bd5b8f1555df88038b3d9
amir <sefati100@gmail.com>
2020-10-31 22:46:02 +0330

    change title master

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>This is master password_get_info</title>
 
 
         
