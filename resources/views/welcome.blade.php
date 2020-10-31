0c733c12818d5c1632f6ba7387456427c74ad833
amir <sefati100@gmail.com>
2020-10-31 22:58:28 +0330

    change title on develop again

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,8 +8,6 @@
 
 
         
-
-        
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
